e1cb965f242712836301f28656059daca54e752a
Pandava919 <pandavapandu919@gmail.com>
2025-02-02 00:10:53 +0530

    removed: authConfig.js from tracking

--- authConfig.js
+++
@@ -1,6 +0,0 @@
-export const msalConfig = {
-    auth: {
-        clindId: import.meta.env.VITE_GOOGLE_CLIENT_ID,
-        authority: "276703981235-d32jf58a1kijh5f8bnd16teca1smirk6.apps.googleusercontent.com"
-    }
-}
